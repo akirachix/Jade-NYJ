142edc6048eb82c57c7be78d3c75626a5b1c9596
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-07-13 21:16:37 +0300

    worked on the podcast

---
+++ jade-multimedia/src/Podcast/index.js
@@ -0,0 +1,75 @@
+import './index.css';
+
+const Podcast = () => {
+ 
+  return (
+    <div className="main-container">
+        
+      <div className="cards-container">
+      <div>
+  <div className="cards-container">
+    <div className='card'>
+    <img src="/image/1podcast.jpg" alt='researcher'/>
+    <div className='content-container'>
+        <p>One year of experience and being  
+          <p>able to see what technologycan do is</p>
+           what keeps me learning and moving in this field.</p>
+        <a href='lorenipsum'>lorenipsum</a>
+
+    </div>
+    </div>
+    <div className='card'>
+    <img src="/image/1podcast.jpg" alt='developer'/>
+    <div className='content-container'>
+        <p>One year of experience and being  
+          <p>able to see what technologycan do is</p>
+           what keeps me learning and moving in this field.</p>
+        <a href='lorenipsum'>lorenipsum</a>
+
+    </div>
+    </div>
+    <div className='card'>
+    <img src="/image/1podcast.jpg" alt='designer'/>
+    <div className='content-container'>
+        <p>One year of experience and being  
+          <p>able to see what technologycan do is</p>
+           what keeps me learning and moving in this field.</p>
+        <a href='lorenipsum'>lorenipsum</a>
+
+    </div>
+    </div>
+   
+
+  </div>  
+  </div>
+      </div>
+      <div className='second-container'>
+   <div className='card'>
+    <img src="/image/1podcast.jpg" alt='designer'/>
+    <div className='content-container'>
+        <p>One year of experience and being  
+          <p>able to see what technologycan do is</p>
+           what keeps me learning and moving in this field.</p>
+        <a href='lorenipsum'>lorenipsum</a>
+
+    </div>
+    </div>
+    <div className='card'>
+    <img src="/image/1podcast.jpg" alt='designer'/>
+    <div className='content-container'>
+        <p>One year of experience and being  
+          <p>able to see what technologycan do is</p>
+           what keeps me learning and moving in this field.</p>
+        <a href='lorenipsum'>lorenipsum</a>
+
+    </div>
+    </div>
+
+
+   </div>
+    
+    </div>
+  );
+};
+
+export default Podcast;
