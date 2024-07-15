f93d7bd7f01084112b1fdf8a7a292247c15382ce
Caren Chepngetich <chepngetichkosgeicarenn@gmail.com>
2024-07-15 06:28:34 +0300

    made changes

---
+++ jade-multimedia/src/LandingPage/index.js
@@ -0,0 +1,29 @@
+import './index.css'
+const LandingPage= ()=>{
+    return(
+        <div>
+          <div className='Container'>
+            <div className='Contour_Action'>
+              <div className='text'>
+          <h1>Welcome To 
+            Our Web
+          </h1>
+          Curate your portfolio with ease and style<br/>
+          Get to know all about "Born a Crime"
+          </div>
+            <div>
+                <button className='StartButton'>Learn More</button>
+            </div>
+            </div>
+            <div>
+            <img className ='image'src="/images/MultiMedia.jpg" alt="logo"/>
+            </div>
+
+        </div>
+        
+       
+        </div>
+    )
+}
+
+export default LandingPage;
