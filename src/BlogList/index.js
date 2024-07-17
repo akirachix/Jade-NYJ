173f4d88fb91a6a08d2c46ed1e5d16ad2a06b0b5
Trecy-Malonza <trecymalonza@gmail.com>
2024-07-17 08:03:08 +0300

    added our blog

--- src/BlogList/index.js
+++ src/BlogList/index.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import BlogPost from '../Post/index.js'; 
 import './index.css';
-import bornacrime from '../images/bornacrime.png';
-import casestudy from '../images/casestudy.png';
+import trevor from '../images/trevor.jpeg'
+import crime from '../images/crime.jpeg'
+// import bornacrime from '../images/bornacrime.png';
+import born from '../images/born.jpeg'
 import PageOne from '../PageOne/index.js';
+import { Link } from 'react-scroll';
+
+<div className='nav-links'>
+    <a className='nav-item'><Link to="Home" smooth={true} duration={500}>Home</Link></a>
+    <a className='nav-item'><Link to="Blog" smooth={true} duration={500}>Blog</Link></a>
+    <a className='nav-item'><Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link></a>
+    <a className='nav-item'><Link to="Podcast" smooth={true} duration={500}>Podcast</Link></a>
+    <a className='nav-item'><Link to="Video" smooth={true} duration={500}>Video</Link></a>
+      </div>
 
 
 
@@ -14,7 +25,7 @@ const posts = [
   },
   {
     title: 'The Impact of Born a Crime',
-    image: bornacrime,
+    image: trevor,
     content: `Born a Crime by Trevor Noah is a powerful memoir that explores themes of racism, identity struggles,
      and resilience. It provides a compelling narrative of Noah's life growing up in South Africa during and after apartheid. 
      The memoir delves into his experiences as a mixed-race child, born to a black Xhosa mother and a white Swiss father, which was a union considered illegal under apartheid laws. Noah's storytelling highlights the
@@ -23,7 +34,7 @@ const posts = [
   {
 
     title: 'Born A Crime',
-    image: bornacrime,
+    image: crime,
     content: `Born a Crime by Trevor Noah remains relevant today as it highlights the impact of racism, 
     identity struggles, and resilience. The memoir sheds light on issues of systemic racism and discrimination, 
     resonating with current social justice movements Born a Crime by Trevor Noah remains relevant today as it highlights the impact of racism, 
@@ -37,7 +48,7 @@ const posts = [
   },
   {
     title: 'Social Issues Arising The Novel Born A Crime',
-    image: bornacrime,
+    image: born,
     content: `Noah eloquently recounts the profound impact of apartheid on his life, 
     reflecting on the segregation and discrimination he faced due to being born to a black Xhosa 
     mother and a white Swiss father. This defiance of apartheid laws is encapsulated in the title "Born a Crime,"
@@ -48,7 +59,7 @@ const posts = [
   },
   {
     title: 'Family and Relationships',
-    image: bornacrime,
+    image: trevor,
     content: `Central to the narrative is Noah's poignant portrayal of his relationship with his mother, Patricia Nombuyiselo Noah. Through vivid storytelling, Noah depicts her as a resilient and influential figure, shaping his values of independence, courage, and compassion. Additionally, the memoir delves into the complexities of Noah's relationship with his father,
      exploring the impact of paternal absence on his understanding of masculinity and the role of fathers
       in shaping their children's lives. Central to the narrative is Noah's poignant portrayal of his relationship with his mother, 
@@ -58,7 +69,7 @@ const posts = [
   },
   {
     title: 'Resilience and Survival',
-    image: bornacrime,
+    image: crime,
     content: `Noah paints a compelling picture of the challenges his family faced while growing up in poverty in Soweto. Despite the hardships, he illustrates how their resilience, resourcefulness, and determination enabled them to overcome adversity. Furthermore, 
     the memoir highlights Noah's adept use of humor as a coping mechanism, revealing his ability to find lightness in the midst of darkness. Noah paints a compelling picture of the challenges his family faced while growing up in poverty in Soweto. Despite the hardships, he illustrates how their resilience, resourcefulness, and determination enabled them to overcome adversity. Furthermore, 
     the memoir highlights Noah's adept use of humor as a coping mechanism, revealing his ability to find lightness in the midst of darkness...`
@@ -100,11 +111,7 @@ const BlogList = () => {
           onBack={currentPostIndex > 0 ? handleBack : null}
         />
       )}
-      <BlogPost
-        post={posts[currentPostIndex]}
-        onNext={currentPostIndex < posts.length - 1 ? handleNext : null}
-        onBack={currentPostIndex > 0 ? handleBack : null}
-      />
+     
 
     </div>
   );
--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -6,10 +6,11 @@ import { Link } from 'react-scroll';
 const Navbar = () => {
   return (
     <div className='nav-links'>
-    <a className='nav-item'><Link to="home" smooth={true} duration={500}>Home</Link></a>
-    <a className='nav-item'><Link to="about" smooth={true} duration={500}>About</Link></a>
-    <a className='nav-item'><Link to="blog" smooth={true} duration={500}>Blog</Link></a>
-    <a className='nav-item'><Link to="contact" smooth={true} duration={500}>Contact</Link></a>
+    <a className='nav-item'><Link to="Home" smooth={true} duration={500}>Home</Link></a>
+    <a className='nav-item'><Link to="Blog" smooth={true} duration={500}>Blog</Link></a>
+    <a className='nav-item'><Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link></a>
+    <a className='nav-item'><Link to="Podcast" smooth={true} duration={500}>Podcast</Link></a>
+    <a className='nav-item'><Link to="Video" smooth={true} duration={500}>Video</Link></a>
       </div>
     
   );
